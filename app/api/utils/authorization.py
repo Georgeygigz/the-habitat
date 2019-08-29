e01d09c2ee1eaa1c598d2cba02d5555a03a8a25a
Georgeygigz <georgeymutti@gmail.com>
2019-08-29 15:46:02 +0300

    feat(social-auth): social authentication:
    - social authentication
    
    [Starts #168192477]

--- app/api/utils/authorization.py
+++ app/api/utils/authorization.py
@@ -2,37 +2,46 @@
 
 from flask_jwt_extended import get_jwt_identity
 from functools import wraps
-
 from app.api.models.auth_modles import User
+
+def get_all_users():
+    users = User.query.all()
+    return users
+
+def check_for_invalid_password_or_email(expression,value):
+    match_value = re.match(expression,value)
+    return match_value
+
 
 def admin_required(func):
     """ Admin Rights."""
     @wraps(func)
     def wrapper_function(*args, **kwargs):
-        users = User.query.all()
+        users = get_all_users()
         try:
             cur_user = [
                 user for user in users if user.email == get_jwt_identity()]
             user_role = cur_user[0].user_type
             if user_role != 'admin':
                 return {
-                    'message': 'This activity can be completed by Admin only'}, 403 # Forbidden
+                    'message': 'This activity can be completed by Admin only'}, 403  # Forbidden
             return func(*args, **kwargs)
         except Exception as e:
             return {"message": e}
     return wrapper_function
 
-
 def store_attendant_required(func):
     """Store attedant rights."""
     @wraps(func)
     def wrapper_function(*args, **kwargs):
-        users = User.query.all()
+        users = get_all_users()
         cur_user = [user for user in users if user.email
                     == get_jwt_identity()]
         user_role = cur_user[0].user_type
-        if user_role != 'attedant': 
+        if user_role != 'attedant':
             return {
-                'message': 'This activity can be completed by Store Attedant only'}, 403 # Forbidden
+                'message': 'This activity can be completed by Store Attedant only'}, 403  # Forbidden
         return func(*args, **kwargs)
     return wrapper_function
+
+
---
+++ instance/social_auth_config.py
@@ -0,0 +1,10 @@
+OAUTH_CREDENTIALS = {
+    'facebook': {
+        'id': '2439383422964869',
+        'secret': '70ad07553ebe298ac8cc04b903b1e422'
+    },
+    'twitter': {
+        'id': '8Orr2HdIe01aSu4COJCkkWzjj',
+        'secret': 'LIpBkwD2jfKLQzYejtb26CqCG5rjKDJ1JU0EEOUnGF0m5BJRyW'
+    }
+}
